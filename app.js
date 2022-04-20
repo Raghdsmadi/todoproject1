af358378ab60bc6b3e3f1d8e15e80da5bccd257f
raghd <raghadmohsmadi@hotmail.com>
2022-04-21 00:29:41 +0300

    task6

--- app.js
+++ app.js
@@ -1,47 +1,68 @@
-var name = window.prompt("Enter your Name: ");
+let answers=[];
+let answer ;
+let name = window.prompt("Enter your Name: ");
+ answers.push(name);
 
-
-
-var gender = window.prompt("Enter your Gender: ");
-var age = window.prompt("Enter your Age: ");
-for (  i=0 ; age <= 0; i++){
-    alert ( "Age must be greater than 0")
-    var age = window.prompt("Enter your Age: ");
+let gender= window.prompt("Enter your Gender: ");
+answers.push(gender);
+answer = window.prompt("Enter your Age: ");
+for (  i=0 ; answer <= 0; i++){
+    answer = window.prompt("Enter your Age: ");
 }
+answers.push(answer);
 
-//result = window.confirm("Do you want to skip welcome message ?");
-
-//var conf=  window.prompt("Do you want to skip welcome message ? please enter yes or no");
-/*var result =window.confirm("Do you want to skip welcome message");
-if (result = 0){*/
-//alert ("Do you want to skip welcome message ? ")
- /*if (gender == 'male')
-        {
-            alert("Welcome, MR. " + name );
-
-        }
-        else if (gender == 'female')
-        {
-            alert("Welcome, MS. " + name);
-
-        } else {
-            alert("Welcome, " + name );
-        }
-*/
 
 if (window.confirm("Do you want to skip welcome message") == false)
  {
             if (gender == 'male')
             {
-                alert("Welcome, MR. " + name );  }
-            else if (gender == 'female')
+                alert("Welcome, MR. " + name);  }
+            else if ( gender == 'female')
             {
                 alert("Welcome, MS. " + name); } 
             else {
                 alert("Welcome, " + name );  }
     
   } 
-  
+
+/******************************************************************************************** */
+ 
+
+    playing(answer);
+    watching(answer);
+    sports(answer);
+    
+
+function playing (answer) {
+ answer= window.prompt("Are you intersting in video games? ");
+invalid(answer) ;
+return answer;
+}
+
+function watching (answer)
+{
+ answer= window.prompt("Are you intersting in watching anime?")
+invalid(answer);
+return answer;
+}
+
+function sports(answer)
+{
+    answer= window.prompt("Are you intersting in Football?")
+    invalid(answer);
+    return answer;
+}
+
+function invalid (answer)
+{
+if (answer === "") {
+    answer="invalid sring"; 
+    answers.push(answer);
+}
+else {answers.push(answer);}
+return answer;
+}
+console.log(answers)
 
 
 
